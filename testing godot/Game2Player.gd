f452d7fe94b022ef667dd45115a77331161bfbff
oliviairl <145864038+oliviairl@users.noreply.github.com>
2023-12-07 17:24:02 -0500

    sigh

--- testing godot/Game2Player.gd
+++ testing godot/Game2Player.gd
@@ -1,15 +1,15 @@
 extends CharacterBody2D
 
-var gravity = 50
-var jumpheight = 15
-var speed = 100
+var gravity = 500
+var jumpheight = 350
+var speed = 300
 
 @onready var cat_player = $AnimationPlayer
 
 
 
 func _physics_process(delta):
-	if Input.is_action_pressed("jump"):
+	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y -=jumpheight
 		
 	velocity.y +=gravity *  delta
@@ -18,7 +18,7 @@ func _process(_delta):
 	velocity.x = speed * Input.get_action_strength("ui_right") - speed * Input.get_action_strength("ui_left")
 	move_and_slide() 
 	if Input.is_action_pressed("ui_right"):
-		cat_player.play("walking")	
+		cat_player.play("walking")
 	else:
 		cat_player.stop()
 	
