d9f1ad029669823e0a3b45202a3dd9acc9d66430
oliviairl <145864038+oliviairl@users.noreply.github.com>
2023-12-06 23:12:59 -0500

    huh.

--- testing godot/Game2Player.gd
+++ testing godot/Game2Player.gd
@@ -4,21 +4,23 @@ var gravity = 100
 var jumpheight = 100
 var speed = 100
 
+@onready var _animation_player = $AnimationPlayer
+
+
 
 func _physics_process(delta):
 	if Input.is_action_pressed("jump"):
 		velocity.y -=jumpheight
 		
 	velocity.y +=gravity *  delta
-func _process(delta):
+func _process(_delta):
 	# Player movement
 	velocity.x = speed * Input.get_action_strength("ui_right") - speed * Input.get_action_strength("ui_left")
 	move_and_slide() 
-	
-	
-	
-	
-	
+	if Input.is_action_pressed("ui_right"):
+		_animation_player.play("walking")	
+	else:
+		_animation_player.stop()
 	
 	
 	
